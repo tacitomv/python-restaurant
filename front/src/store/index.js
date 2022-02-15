56305005f26ff1fbb7abea396d484fa67cf76c2b
Tácito Viero <tacito.viero@gmail.com>
2022-02-15 18:29:13 -0300

    cart quantity update

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -10,6 +10,21 @@ export default createStore({
     UPDATE_PRODUCT_ITEMS(state, payload) {
       state.productItems = payload;
     },
+    REMOVE_CART_ITEMS(state, productItem) {
+      state.cartItems.map(cartProduct => {
+        if (cartProduct.id === productItem.id && cartProduct.quantity > 1) {
+          cartProduct.quantity--;
+        } else if (
+          cartProduct.id === productItem.id &&
+          cartProduct.quantity === 1
+        ) {
+          const cartIndexToRemove = state.cartItems.findIndex(
+            cartProduct => cartProduct.id === productItem.id
+          );
+          state.cartItems.splice(cartIndexToRemove, 1);
+        }
+      });
+    },
     ADD_CART_ITEMS(state, newCartProduct) {
       let cartProductExists = false;
       state.cartItems.map(cartProduct => {
@@ -24,6 +39,9 @@ export default createStore({
         state.cartItems.push(newCartProduct);
       }
     },
+    DELETE_CART_ITEMS(state, newCart) {
+      state.cartItems = newCart;
+    },
   },
   actions: {
     getProductItems({ commit }) {
@@ -35,10 +53,10 @@ export default createStore({
       commit("ADD_CART_ITEMS", cartItem);
     },
     removeCartItem({ commit }, cartItem) {
-      commit("UPDATE_CART_ITEMS", cartItem);
+      commit("REMOVE_CART_ITEMS", cartItem);
     },
     removeAllCartItems({ commit }) {
-      commit("UPDATE_CART_ITEMS", {});
+      commit("DELETE_CART_ITEMS", {});
     },
   },
   getters: {
@@ -55,7 +73,10 @@ export default createStore({
         .toFixed(2);
     },
     cartQuantity: state => {
-      return state.cartItems.length;
+      return state.cartItems
+        .reduce((acc, cartItem) => {
+          return cartItem.quantity + acc;
+        }, 0);
     },
   },
 });
